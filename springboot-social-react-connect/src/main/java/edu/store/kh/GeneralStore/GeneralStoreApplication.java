e31484ba8b4ade5e1b769b54438f8f4242c76a26
Kyungnan Kim <kht241022@gmail.com>
2025-02-17 09:52:21 +0900

    User Role Provider mapper dto setting

---
+++ springboot-social-react-connect/src/main/java/edu/store/kh/GeneralStore/GeneralStoreApplication.java
@@ -0,0 +1,14 @@
+package edu.store.kh.GeneralStore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeneralStoreApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(GeneralStoreApplication.class, args);
+	}
+
+}
